7dab5bf0ac0e9a72d47925488bea20c1113b0372
mhorton <horton-michael@users.noreply.github.com>
2023-09-21 15:03:22 -0400

    fixes apiRoutes import

---
+++ routes/api/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const router = require("express").Router();
+
+// route collections go here
+
+// API ROUTES
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const htmlRoutes = require("./htmlRoutes.js");
+const apiRoutes = require("./API/index.js");
+// HTML ROUTES
+
+// API ROUTES
+router.use(htmlRoutes);
+router.use("/api", apiRoutes);
 
 module.exports = router;
